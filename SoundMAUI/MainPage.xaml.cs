1e2ae34d94032bb223e27e3ff8e63f61f599ec5b
Guvanch Yazgulyyev <77998139+GuvanchYazgulyyev@users.noreply.github.com>
2024-03-17 02:56:09 +0300

    Add project files.

---
+++ SoundMAUI/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Plugin.Maui.Audio;
+
+namespace SoundMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        private readonly IAudioManager audioManager;
+        public MainPage(IAudioManager audioManager)
+        {
+            InitializeComponent();
+            this.audioManager = audioManager;
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            var play = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("music.mp4"));
+            play.Play();
+        }
+    }
+
+}
